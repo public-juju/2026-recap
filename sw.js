0b2ef6a1ed861a28e068aa5e88d3b6745652fdaa
public-juju <0610294@gmail.com>
2026-07-25 20:31:42 +0900

    Use distinct pastel palette for broadcaster badges (sw.js)

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "recap2026-shell-v8";
+const CACHE_NAME = "recap2026-shell-v9";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=10",
-  "./app.js?v=10",
-  "./data.js?v=10",
-  "./config.js?v=10",
+  "./style.css?v=11",
+  "./app.js?v=11",
+  "./data.js?v=11",
+  "./config.js?v=11",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"

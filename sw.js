9b86517040f95f72c4a762ce3370e03059aff090
public-juju <0610294@gmail.com>
2026-08-17 10:21:32 +0900

    Remove genre everywhere + auto-clear existing genre data (sw.js)

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "recap2026-shell-v21";
+const CACHE_NAME = "recap2026-shell-v22";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=23",
-  "./app.js?v=23",
-  "./data.js?v=23",
-  "./config.js?v=23",
+  "./style.css?v=24",
+  "./app.js?v=24",
+  "./data.js?v=24",
+  "./config.js?v=24",
   "./manifest.json",
   "./icons/icon-192.png",
   "./icons/icon-512.png"

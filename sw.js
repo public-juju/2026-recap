7bdceb25cb83ec26712248c737bbf4e2dd714782
public-juju <0610294@gmail.com>
2026-08-17 11:29:31 +0900

    Fix broken map: restore original path data, mask island safely instead (sw.js)

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "recap2026-shell-v34";
+const CACHE_NAME = "recap2026-shell-v35";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=36",
-  "./app.js?v=36",
-  "./data.js?v=36",
-  "./config.js?v=36",
+  "./style.css?v=37",
+  "./app.js?v=37",
+  "./data.js?v=37",
+  "./config.js?v=37",
   "./manifest.json",
   "./korea-map.svg",
   "./icons/icon-192.png",

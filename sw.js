762ecabc2c650c82481706492d013a49c660e14c
public-juju <0610294@gmail.com>
2026-08-17 11:17:01 +0900

    Distinct muted pastel per region + custom destination labels on map (sw.js)

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = "recap2026-shell-v31";
+const CACHE_NAME = "recap2026-shell-v32";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./style.css?v=33",
-  "./app.js?v=33",
-  "./data.js?v=33",
-  "./config.js?v=33",
+  "./style.css?v=34",
+  "./app.js?v=34",
+  "./data.js?v=34",
+  "./config.js?v=34",
   "./manifest.json",
   "./korea-map.svg",
   "./icons/icon-192.png",
